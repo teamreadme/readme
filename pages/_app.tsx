761dbd0427daa572ea527005a4c9f58220bb071a
Jake Reynolds <jreynoldsdev@gmail.com>
2022-03-27 15:06:00 -0500

    Fixing title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { Head } from "next/document";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
