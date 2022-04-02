e5757fa6a07a800b39db0d6bdfe8e5223acf06b4
Jake Reynolds <jreynoldsdev@gmail.com>
2022-04-02 14:22:11 -0500

    Attempting icon fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons&display=optional" rel="stylesheet" />
         </Head>
         <body>
           <Main />
