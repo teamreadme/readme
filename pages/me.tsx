a6da3b875915ffaf8d617597cf6433d3c9365deb
Jake Reynolds <jreynoldsdev@gmail.com>
2022-04-02 14:15:30 -0500

    adding prisma import

--- pages/me.tsx
+++ pages/me.tsx
@@ -1,6 +1,6 @@
 import { redirectToLogin } from "@/utils/apiUtils";
 import { getSession } from "next-auth/react";
-import Link from "next/link";
+import { prisma } from "@/utils/prisma";
 
 export default function Me() {
   return <div></div>;
