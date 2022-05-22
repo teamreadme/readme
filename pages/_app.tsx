bce4beb1a9c71eb71d1115df165c877e2311ad86
Jake Reynolds <jreynoldsdev@gmail.com>
2022-05-22 14:22:38 -0500

    meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
       <Head>
-      <title key="head-title">README</title>
-      <meta name="description" content="Let the world know who you are" />
+        <title key="head-title">README</title>
+        <meta name="description" content="Let the world know who you are" />
         <meta property="og:title" content="README" key="ogtitle" />
         <meta property="og:description" content="Let the world know who you are" key="ogdesc" />
       </Head>
