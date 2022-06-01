16d03b68f9837863831e1fa43964af8a759d04cf
Jake Reynolds <jreynoldsdev@gmail.com>
2022-05-31 20:50:24 -0500

    fixing migration

--- prisma/migrations/20220601003950_readme_count/migration.sql
+++ prisma/migrations/20220601003950_readme_count/migration.sql
@@ -1,7 +1,7 @@
 -- AlterTable
 ALTER TABLE "read_me" ADD COLUMN     "reads" INTEGER NOT NULL DEFAULT 0;
-update "read_me" set reads=323 where username='jreynoldsdev';
-update "read_me" set reads=20 where username='cody';
-update "read_me" set reads=5 where username='ryan_tinklenberg';
-update "read_me" set reads=5 where username='jekrch';
-update "read_me" set reads=10 where username='dajacks';
+update "read_me" set reads=323 from users u where u.username='jreynoldsdev' and u.id=user_id;
+update "read_me" set reads=20  from users u where u.username='cody' and u.id=user_id;
+update "read_me" set reads=5  from users u where u.username='ryan_tinklenberg' and u.id=user_id;
+update "read_me" set reads=5  from users u where u.username='jekrch' and u.id=user_id;
+update "read_me" set reads=10  from users u where u.username='dajacks' and u.id=user_id;
