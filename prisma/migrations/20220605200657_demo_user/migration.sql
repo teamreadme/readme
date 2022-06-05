9602bea7a7b7e555ecd744167253f54db0651a5d
Jake Reynolds <jreynoldsdev@gmail.com>
2022-06-05 15:27:44 -0500

    fixing demo user migration

--- prisma/migrations/20220605200657_demo_user/migration.sql
+++ prisma/migrations/20220605200657_demo_user/migration.sql
@@ -1,7 +1,7 @@
 insert into users (id, first_name, last_name, email, username, "emailVerified") values (
-    'demouserid','Jake', 'Reynolds', 'jake@readmefirst.co', 'jreynoldsdev', now()
+    'demouserid','Jake', 'Reynolds', 'jake@readmefirst.co', 'jreynoldsdevdemo', now()
 );
 
 insert into read_me (id, "user_id", "text", reads) values (
-    'demoreadmeid', (select id from users where username='jreynoldsdev'), '<h1>Hello!</h1> Checkout `.env` to update the default inspiration README', 100
+    'demoreadmeid', (select id from users where username='jreynoldsdevdemo'), '<h1>Hello!</h1> Checkout `.env` to update the default inspiration README', 100
 );
--- prisma/migrations/20220605200658_demo_user/migration.sql
+++
@@ -1,1 +0,0 @@
-update read_me set text='<h1>Hello!</h1> Checkout `.env` to update the default inspiration README' where id='demoreadme';
