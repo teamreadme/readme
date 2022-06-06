3c7f2513f0e1c3fa6bb70908c56e939e67e47ae7
Jake Reynolds <jreynoldsdev@gmail.com>
2022-06-06 13:51:41 -0500

    fixing CSP

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const securityHeaders = [{
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ export default function Home({
     const [email, setEmail] = useState<string>();
     const [error, setError] = useState<string>();
     const router = useRouter();
-    async function signInClicked(e: any) {
+    async function registerClicked(e: any) {
         e.preventDefault();
         try {
             setRegistering(true);
@@ -69,11 +69,8 @@ export default function Home({
         <>
             <AppNav authenticated={userSession != null} />
             <div className="relative lg:mb-32 lg:flex bg-gray-100 overflow-hidden">
-
                 <div className="max-w-7xl mx-auto">
                     <div className="relative z-10 pb-8 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-
-
                         <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                             <div className="sm:text-center lg:text-left">
                                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
@@ -85,7 +82,7 @@ export default function Home({
                                 </p>
                                 <p className="text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:hidden mt-8">How does it work? Check out an example <Link href={`/${process.env.NEXT_PUBLIC_INSPIRATION_USERNAME}`}>here!</Link></p>
                                 <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                                    <form onSubmit={signInClicked} className="mt-3 sm:flex">
+                                    <form onSubmit={registerClicked} className="mt-3 sm:flex">
                                         <label htmlFor="email" className="sr-only">
                                             Email
                                         </label>
@@ -100,7 +97,6 @@ export default function Home({
                                         <button
                                             type="submit"
                                             disabled={registering}
-                                            onClick={signInClicked}
                                             className={classNames("mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-800 shadow-sm hover:bg-purple-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto",
                                                 { 'bg-purple-300': registering })}
                                         >
@@ -178,8 +174,10 @@ export default function Home({
                             <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
                                 <Image
                                     layout="fill"
-                                    height="2408"
-                                    width="1595"
+                                    /* Eager loading, because lazy loading requires the use of an inline data: placeholder that violates our CSP */
+                                    loading="eager"
+                                    lazyRoot={null}
+                                    alt="Image of a city's skyline"
                                     className="absolute inset-0 h-full w-full object-cover"
                                     src="/skyline.jpg"
                                 />
