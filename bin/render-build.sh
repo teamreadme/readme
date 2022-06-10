28cab84a4d6a16e7709e81d129f9198d88272987
Jake Reynolds <jreynoldsdev@gmail.com>
2022-06-10 17:13:33 -0500

    updating build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+echo "Installing dependencies"
+npm install ci
+
+echo "Building app"
+npm run build
+
+echo "Migrating database" #https://community.render.com/t/release-command-for-db-migrations/247
 npx prisma migrate deploy
